be391ade0b3442d0eaf073e913b0d798ebab0eb5
João Macieira <jp.macieira@uol.com.br>
2021-09-26 16:08:03 -0300

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.listen(port, () => console.log(`Server started at ${port}`));
